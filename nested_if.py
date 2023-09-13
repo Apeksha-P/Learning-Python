5cd14239790bcdced9ed7815d0f5e84b4b15f427
H.P.P. Apeksha <118092627+Apeksha60@users.noreply.github.com>
2023-09-13 21:09:52 +0530

    Enter number is one, two, three, four or greater than 4

---
+++ nested_if.py
@@ -0,0 +1,14 @@
+number = int(input("Enter number: "))
+if number==1:
+    print("One")
+else:
+    if number==2:
+        print("Two")
+    else:
+        if number==3:
+            print("Three")
+        else:
+            if number==4:
+                print("Four")
+            else:
+                print("Number is greater than 4")
