feeb5c2e16b28ed8fc40326fd74d2dde3295111b
H.P.P. Apeksha <118092627+Apeksha60@users.noreply.github.com>
2023-09-28 16:16:01 +0530

    Sets in python
    Sets in Python
    {1, 2, 3, 4}
    <class 'set'>
    
    Adding elements to set
    {1, 2, 3, 4, 5}
    union of set
    {1, 2, 3, 4, 5, 6, 9}
    Difference of set
    {1, 4, 5}

---
+++ set.py
@@ -0,0 +1,12 @@
+print("Sets in Python")
+s1={1,2,3,4}
+print(s1)
+print(type(s1))
+print("\nAdding elements to set")
+s1.add(5)
+print(s1)
+s2={2,3,6,9}
+print("union of set")
+print(s1.union(s2))
+print("Difference of set")
+print(s1.difference(s2))
