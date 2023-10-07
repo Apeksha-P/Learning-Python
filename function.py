63971a52676dee7a15ba8ed3fa6b0c45b43b24be
H.P.P. Apeksha <118092627+Apeksha60@users.noreply.github.com>
2023-10-07 08:56:59 +0530

    calcutate some operations
    Enter number 1: 45
    Enter number 2: 20
    Sum =  65
    Sub =  25
    Div =  2.25
    Mul =  900

---
+++ function.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    print("Sum = ",x+y)
+def sub(x,y):
+    print("Sub = ",x-y)
+def div(x,y):
+    print("Div = ",x/y)
+def mul(x,y):
+    print("Mul = ",x*y)
+    
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+add(a,b)
+sub(a,b)
+div(a,b)
+mul(a,b)
